9e8fbc385bc01ea8ac3a827577c7da868c8ef436
HASAN RAZA <92hasanraza689@gmail.com>
2025-05-20 22:19:44 +0500

    updates

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Login  = () => {
+  return (
+    <div className='flex justify-center items-center h-[100vh]'>
+        <Button>Login</Button>
+    </div>
+  )
+}
+
+export default Login 
